59df1309e12268871dd24318f874765179cf283b
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2017-01-02 11:46:03 +0100

    feat(app): add yo and jhipster logic

--- generators/app/index.js
+++ generators/app/index.js
@@ -25,45 +25,13 @@ module.exports = yeoman.Base.extend({
     },
     displayLogo: function () {
       // Have Yeoman greet the user.
-      this.log('Welcome to the ' + chalk.red('JHipster compodoc') + ' generator! ' + chalk.yellow('v' + packagejs.version + '\n'));
+      this.log('Welcome to the JHipster compodoc generator! ' + chalk.blue('v' + packagejs.version + '\n'));
     }
-  },
-
-  prompting: function () {
-    var done = this.async();
-
-    var prompts = [{
-      type: 'input',
-      name: 'message',
-      message: 'Please put something',
-      default: 'hello world!'
-    }];
-
-    this.prompt(prompts, function (props) {
-      this.props = props;
-      // To access props later use this.props.someOption;
-
-      done();
-    }.bind(this));
   },
 
   writing: {
     writeTemplates : function () {
-      this.baseName = jhipsterVar.baseName;
-      this.packageName = jhipsterVar.packageName;
-      this.angularAppName = jhipsterVar.angularAppName;
-      var javaDir = jhipsterVar.javaDir;
-      var resourceDir = jhipsterVar.resourceDir;
-      var webappDir = jhipsterVar.webappDir;
-
-      this.message = this.props.message;
-
-      this.log('baseName=' + this.baseName);
-      this.log('packageName=' + this.packageName);
-      this.log('angularAppName=' + this.angularAppName);
-      this.log('message=' + this.message);
-
-      this.template('dummy.txt', 'dummy.txt', this, {});
+      jhipsterFunc.addNpmDevDependency('compodoc', '0.0.26');
     },
 
     registering: function () {
@@ -76,10 +44,10 @@ module.exports = yeoman.Base.extend({
   },
 
   install: function () {
-    this.installDependencies();
+    this.npmInstall();
   },
 
   end: function () {
-    this.log('End of compodoc generator');
+    this.log('End of JHipster compodoc generator');
   }
 });
